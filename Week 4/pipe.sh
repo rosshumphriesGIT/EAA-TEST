76795e878291cdbef6588ea1aac0282823d7dc93
John Burns <jzburns@gmail.com>
2016-10-13 10:33:50 +0100

    pipe.sh/cntlm

---
+++ Week 4/pipe.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#grep searches for a patten in a line of text my sql
+# ps -ef prints a snapshot of every process in the system 
+#where does the txt comefrom? from the ps -ef |
+
+#this new command:
+#searches all the running processes looking for the mysql process
+ps -ef | grep cntlm
